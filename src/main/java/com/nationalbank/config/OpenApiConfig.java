2ce28d34c2d84a2ceebd2535e666de49e2c84c20
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2025-05-07 17:29:19 +0200

    Adding variables in .env

---
+++ src/main/java/com/nationalbank/config/OpenApiConfig.java
@@ -0,0 +1,45 @@
+package com.nationalbank.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("National Bank System API")
+                        .description("REST API for National Bank of Rwanda Banking System")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("National Bank of Rwanda")
+                                .email("support@nationalbank.rw")
+                                .url("https://www.nationalbank.rw"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server()
+                                .url("/api")
+                                .description("API Server")
+                ));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+} 
